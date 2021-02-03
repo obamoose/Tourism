bf24babe5283e145ee10dfbe47b5babd81ea0932
obamoose <obamoose@gmail.com>
2021-02-03 21:01:20 +0100

    initial fixes

---
+++ Husky NPC Mod (Use This Folder)/[CP] Husky/assets/Unused/spring_outdoorsTileSheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="spring_outdoorsTileSheet" tilewidth="32" tileheight="32" tilecount="468" columns="12">
+ <image source="../../../../../../../../Users/elphi/Documents/Stardew Valley/Modding/New folder/spring_outdoorsTileSheet.png" width="400" height="1264"/>
+</tileset>
---
+++ Husky NPC Mod (Use This Folder)/[CP] Husky/assets/Unused/townInterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="townInterior" tilewidth="32" tileheight="32" tilecount="544" columns="16">
+ <image source="../../../../../../../../Users/elphi/Documents/Stardew Valley/Modding/New folder/townInterior.png" width="512" height="1088"/>
+</tileset>
