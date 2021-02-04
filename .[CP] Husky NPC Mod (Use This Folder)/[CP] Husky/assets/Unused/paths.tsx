afe200a3c443783ccd4af6f353a95cfc3ea26ab3
obamoose <obamoose@gmail.com>
2021-02-04 21:04:55 +0100

    Clear NPC files

--- .[CP] Husky NPC Mod (Use This Folder)/[CP] Husky/assets/Unused/paths.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="paths" tilewidth="16" tileheight="16" tilecount="32" columns="4">
- <image source="../../../../../../../../Users/elphi/Documents/Stardew Valley/Modding/New folder/paths.png" width="64" height="128"/>
-</tileset>
